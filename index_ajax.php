7eeed8690a6681b496f09201935b2bf7696890cf
seryakovda <137743304+seryakovda@users.noreply.github.com>
2026-02-16 11:55:02 +0700

    Доработка1

--- index_ajax.php
+++ index_ajax.php
@@ -1,13 +1,19 @@
 <?php
-require "Properties/error_reporting.php";
-
 /**
  * Является единственной точкой входа для всех инструкциф приложения
  */
 
+require "Properties/error_reporting.php";
+/**
+ * spl_autoload_register.php Содержит
+ * функцию spl_autoload_register которая автоматически подгружает клас по его nameSpace
+ */
+require "spl_autoload.php";
+
+
 set_time_limit(0);
 
-
+\models\ErrorLog::saveError(date('d.m.Y H:i:s')."Данные пришли в точку входа",'log.txt');
 if (array_key_exists('sessionHandle',$_REQUEST)){
     session_id($_REQUEST['sessionHandle']);
 }
@@ -16,11 +22,7 @@ if (array_key_exists('sessionHandle',$_REQUEST)){
 
 session_start();
 
-/**
- * spl_autoload_register.php Содержит
- * функцию spl_autoload_register которая автоматически подгружает клас по его nameSpace
- */
-require "spl_autoload.php";
+
 
 /**
  * Создаётся и исполняется основной контроллер (маршрутизатор) всего приложения
